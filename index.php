b11aa2cb277b835f118980409e14feb3e434ee72
zhunengwei <459818128@qq.com>
2017-07-21 00:38:49 +0800

    骨架

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    //加载配置文件
+    include("moonmoon.conf");
+    //加载全局函数库
+    include(MM_PATH."/Common/functions.inc");
+    //加载所有的父类
+    include(MM_PATH."/MVC/Controller/CAfather.inc");
+    //加载参数所代表的Controller
+    //include();
+    
+?>
